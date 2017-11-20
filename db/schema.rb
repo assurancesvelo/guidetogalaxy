20ee8f608736ab45cbc7b1dc12877b6087e11db5
assurancesvelo <assurancesvelo@gmail.com>
2017-11-20 11:55:06 +0100

    d
    d

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171025162452) do
+ActiveRecord::Schema.define(version: 20171106125615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,45 @@ ActiveRecord::Schema.define(version: 20171025162452) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "cyclos", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.string "date"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "etape"
+    t.string "url"
+    t.string "distance"
+    t.string "vae"
+    t.string "velocourse"
+  end
+
+  create_table "lots", force: :cascade do |t|
+    t.string "image"
+    t.string "brand"
+    t.string "description"
+    t.string "recompense"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "marques", force: :cascade do |t|
+    t.string "logo"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recomandations", force: :cascade do |t|
+    t.string "name"
+    t.string "statut"
+    t.string "view"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "photo"
   end
 
   create_table "users", force: :cascade do |t|
