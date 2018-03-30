e2b728a95443d7fd10cdbf2dcfda6a5afdd73cb1
assurancesvelo <assurancesvelo@gmail.com>
2018-03-30 13:47:41 +0200

    d
    d

--- app/controllers/cours_controller.rb
+++ app/controllers/cours_controller.rb
@@ -2,37 +2,5 @@ class CoursController < ApplicationController
   def index
   end
 
-  def prototyping194V3
-  end
-
-
-  def prototyping278g9
-  end
-
-
-  def statistiques975
-  end
-
-  def aarrr256
-  end
-
-  def googleanalytics9565
-  end
-
-  def pmf46292
-  end
-
-  def adwords876T
-  end
-
-  def retargetingZW678
-  end
-
-  def facebook666S
-  end
-
-
-  def growth8933G3
-  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,17 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :cours, only: [:index]
-  get "/aarrr256" => "cours#aarrr256"
-  get "/statistiques975" => "cours#statistiques975"
-  get "/prototyping194V3" => "cours#prototyping194V3"
-  get "/prototyping278g9" => "cours#prototyping278g9"
-  get "/googleanalytics9565" => "cours#googleanalytics9565"
-  get "/facebook666S" => "cours#facebook666S"
-  get "/adwords876T" => "cours#adwords876T"
+  get "/aarrr256" => "pages#aarrr256"
+  get "/statistiques975" => "pages#statistiques975"
+  get "/prototyping194V3" => "pages#prototyping194V3"
+  get "/prototyping278g9" => "pages#prototyping278g9"
+  get "/googleanalytics9565" => "pages#googleanalytics9565"
+  get "/facebook666S" => "pages#facebook666S"
+  get "/adwords876T" => "pages#adwords876T"
   get "/form" => "pages#form"
 
-  get "/pmf46292" => "cours#pmf46292"
-  get "/growth8933G3" => "cours#growth8933G3"
+  get "/pmf46292" => "pages#pmf46292"
+  get "/growth8933G3" => "pages#growth8933G3"
 
-  get "/retargetingZW678" => "cours#retargetingZW678"
+  get "/retargetingZW678" => "pages#retargetingZW678"
 end
